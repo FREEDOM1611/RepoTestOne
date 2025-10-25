70796223e0154a6510f83b54c987b3dcdd3a47b1
Suheiho <freeforallniisaac@gmail.com>
2025-10-25 17:06:56 +0800

    Added basic addition and subtraction

--- test.java
+++ test.java
@@ -1,6 +1,18 @@
 public class test {
     public static void main(String[] args){
         System.out.println("Hello, World!");
+
+        int a =5;
+        int b =10;
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+
+        int c = 1000;
+        int d = 2500;
+        int total = d -c;
+
+        System.out.println("Total: " + total);
     }
     
 }
